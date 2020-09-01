20615304a2f7ce4c2ea065c604d1017ec6f73551
Becavalier <yhorg@hotmail.com>
2020-08-31 23:56:09 -0400

    Add the capabilities of reading and executing word.

--- vm.asm
+++ vm.asm
@@ -10,19 +10,24 @@ codes:
     db '0123456789ABCDEF'  ; char symbols;
 
 section .bss
-resb 1023  ; high addr;
+resb 1024  ; high addr;
 rstack_start:
+input_buf: resb 1024
 
 global _start
 section .text
 
 main_stub:
-    dq xt_dup
-    dq xt_plus
-    dq xt_dbl
-    dq xt_print_top_stack
+    dq xt_read_word
+    dq xt_eval_word
     dq xt_exit
-  
+
+repl_stub:
+    dq xt_read_word
+    dq xt_eval_word
+    ; dq xt_repl_reset
+    dq xt_exit
+
 ; dictionary (words);
 nw_init:
     dq 0
@@ -81,7 +86,7 @@ xt_exit:
 
 ; double;
 nw_dbl:
-    dq nw_dbl
+    dq nw_exit
     db 'dbl', 0
     db 0
 xt_dbl:
@@ -90,10 +95,56 @@ xt_dbl:
     dq xt_plus
     dq xt_ret
 
+; read_word;
+nw_read_word:
+    dq nw_dbl
+    db 'read_word', 0
+    db 0
+xt_read_word:
+    dq impl_read_word
+
+; eval_word;
+nw_eval_word:
+    dq nw_read_word
+    db 'eval_word', 0
+    db 0
+xt_eval_word:
+    dq impl_eval_word
+    
 _nw_head:
-    dq nw_exit
+    dq nw_eval_word
 
 ; implementations;
+impl_eval_word:
+    mov r8, _nw_head
+.forward:
+    mov r9, -1
+    mov r8, [r8]  ; start looking;
+    cmp r8, 0
+    je .end
+    mov r10, r8
+    add r10, 8
+.loop:
+    inc r9
+    cmp byte[r10 + r9], 0
+    je .eval
+    mov bpl, [input_buf + r9]
+    sub bpl, [r10 + r9]
+    jz .loop
+    jmp .forward
+.eval:
+    ; eval;
+    jmp [r10 + r9 + 2]
+.end:
+    jmp next
+
+impl_read_word:
+.read:
+    sys_read_stdin input_buf, 32
+    cmp rax, 1
+    jle .read
+    jmp next
+
 impl_init:
     mov rstack, rstack_start  ; stack holding old outer pc;
     mov pc, main_stub
